d2a675a43e34bdb7fb496d85fb9c146a387bc5a3
Danilo Xiaoju Zhu <zhuxiaoj1@outlook.com>
2022-02-09 10:16:41 -0500

    Create Java Maven project

---
+++ importer/src/main/java/utils/App.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * @since 2022-02-09
+ * @version 1.0
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
