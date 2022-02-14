6800e033441e3343193ee6d588b4b9cb212b5749
Danilo Xiaoju Zhu <zhuxiaoj1@outlook.com>
2022-02-14 15:42:27 -0500

    Add moviedb schemas

--- dmdb-app/database/mongodb.js
+++ dmdb-app/database/mongodb.js
@@ -2,6 +2,7 @@
  * @author Danilo Zhu
  */
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 require('dotenv').config();
 
 main().catch(err => console.err(err));
@@ -9,3 +10,34 @@ main().catch(err => console.err(err));
 async function main() {
     await mongoose.connect(process.env.MONGODB);
 }
+
+const movie = new Schema({
+    title: String,
+    description: String,
+    duration: String,
+    genre: String,
+    rating: Number,
+    poster: String
+});
+
+const submission = new Schema({
+    title: String,
+    description: String,
+    duration: String,
+    genre: String,
+    rating: Number,
+    poster: String,
+    link: String
+});
+
+const review = new Schema({
+    username: String,
+    content: String,
+    datePosted: String
+});
+
+const user = new Schema({
+    username: String,
+    email: String,
+    password: String
+});
