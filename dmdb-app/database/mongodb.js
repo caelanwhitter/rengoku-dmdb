b559d341181d81683dedefde1649ced8e315a109
Mikael Baril <mikael.baril@dawsoncollege.qc.ca>
2022-02-17 15:37:21 -0500

    Created a movie schema and model. Added connection from mongoose to mongoDb

--- dmdb-app/database/mongodb.js
+++ dmdb-app/database/mongodb.js
@@ -5,39 +5,53 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 require('dotenv').config();
 
-main().catch(err => console.err(err));
+mongoose.connect(process.env.ATLAS_URL);
+//Get the default connection
+var db = mongoose.connection;
 
-async function main() {
-    await mongoose.connect(process.env.MONGODB);
-}
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-const movie = new Schema({
-    title: String,
+const movieSchema = new Schema({
     description: String,
+    director:String,
     duration: String,
     genre: String,
-    rating: Number,
-    poster: String
-});
-
-const submission = new Schema({
-    title: String,
-    description: String,
-    duration: String,
-    genre: String,
-    rating: Number,
+    gross: String,
     poster: String,
-    link: String
-});
+    rating: String,
+    releaseYear: Number,
+    score: Number,
+    title: String,
+}, {collection: 'movies'});
 
-const review = new Schema({
-    username: String,
-    content: String,
-    datePosted: String
-});
+const Movie = mongoose.model('Movie', movieSchema)
+const queryMovies =  Movie.find();
+console.log(queryMovies);
 
-const user = new Schema({
-    username: String,
-    email: String,
-    password: String
-});
+//movie.findOne(function(error, result){});
+
+// const submission = new Schema({
+//     description: String,
+//     director:String,
+//     duration: String,
+//     genre: String,
+//     gross: String,
+//     poster: String,
+//     rating: String,
+//     releaseYear: Number,
+//     score: Number,
+//     title: String,
+// });
+
+// const review = new Schema({
+//     username: String,
+//     content: String,
+//     datePosted: String
+// });
+
+// const user = new Schema({
+//     username: String,
+//     email: String,
+//     password: String
+// });
