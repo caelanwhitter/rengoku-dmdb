e8242aac30e2b91279035bfe9a6bff0468ef628c
Danilo Xiaoju Zhu <zhuxiaoj1@outlook.com>
2022-02-18 09:47:19 -0500

    Refactor code and add button for reviews

--- dmdb-app/src/routes/movie.js
+++ dmdb-app/src/routes/movie.js
@@ -1,33 +1,14 @@
 import { NavLink, useParams } from "react-router-dom";
-import { getMovies } from "../data";
+import { Button } from "@mantine/core";
 
 export default function Movie() {
   let params = useParams();
-  let movies = getMovies();
+
   return (
-    <div>
-    <h2>This is the ID of the movie: {params.movieId}</h2>
-    <h3>This is the name of the movie: {params.movieName}</h3>
-    
-    <nav style={{
-        borderRight: "solid 1px",
-        padding: "1rem"
-      }}>
-        {movies.map(movies => (
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                margin: "1rem 0",
-                color: isActive ? "red" : "blue"
-              };
-            }} to={`/movies/${movies.number}/reviews`}
-            key={movies.number}
-          >
-             {movies.name}
-          </NavLink>
-        ))}
-      </nav>
+    <div id="movieDetails">
+      <h2>This is the ID of the movie: {params.movieId}</h2>
+      <h3>This is the name of the movie: {params.movieName}</h3>
+      <Button variant="subtle" component={NavLink} to={`/movies/${params.movieId}/reviews`}>View Reviews</Button>
     </div>
-      );
+    );
 }
