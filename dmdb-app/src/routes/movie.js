80157f7040d5b7d890bd7979eb1e411a973d2983
Miky Baril <miky.baril1@gmail.com>
2022-02-21 12:37:59 -0500

    Pulled latest code from mantine and mongoose-setup

--- dmdb-app/src/routes/movie.js
+++ dmdb-app/src/routes/movie.js
@@ -5,37 +5,10 @@ export default function Movie() {
   let params = useParams();
 
   return (
-<<<<<<< HEAD
-    <div>
-      
-    <h2>This is the ID of the movie: {params.movieId}</h2>
-    <h3>This is the name of the movie: {params.movieName}</h3>
-    
-    <nav style={{
-        borderRight: "solid 1px",
-        padding: "1rem"
-      }}>
-        {movies.map(movies => (
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                margin: "1rem 0",
-                color: isActive ? "red" : "blue"
-              };
-            }} to={`/movies/id_`+`${movies.number}/reviews`}
-            key={movies.number}
-          >
-             {movies.name}
-          </NavLink>
-        ))}
-      </nav>
-=======
     <div id="movieDetails">
       <h2>This is the ID of the movie: {params.movieId}</h2>
       <h3>This is the name of the movie: {params.movieName}</h3>
       <Button variant="subtle" component={NavLink} to={`/movies/${params.movieId}/reviews`}>View Reviews</Button>
->>>>>>> e8242aac30e2b91279035bfe9a6bff0468ef628c
     </div>
     );
 }
