40061718f2705b8ab74a8713d8b7b75572a1792e
Daniel Lam <lam.daniel.123@hotmail.com>
2022-02-21 15:22:45 -0500

    Add pagination skeleton

--- dmdb-app/src/routes/Movies.js
+++ dmdb-app/src/routes/Movies.js
@@ -1,40 +1,43 @@
 import { NavLink, Outlet } from "react-router-dom";
 import { getMovies } from "../data";
-import { Table } from '@mantine/core';
+import { Table, Pagination } from '@mantine/core';
+import { useState } from 'react';
 
 export default function Movies() {
-  let movies = getMovies();
+    let movies = getMovies();
 
-  const rows = movies.map((element) => (
-    <tr key={element.number}>
-      <td><NavLink style={({ isActive }) => {
-            return { color: isActive ? "red" : "blue" };
-          }} to={`/movies/${element.number}`}
-          key={element.number}>
-          {element.name}</NavLink></td>
-      <td>{element.amount}</td>
-      <td>{element.due}</td>
-    </tr>
-  ));
+    const [activePage, setPage] = useState(1);
+    const rows = movies.map((element) => (
+        <tr key={element.number}>
+            <td><NavLink style={({ isActive }) => {
+                return { color: isActive ? "red" : "blue" };
+            }} to={`/movies/${element.number}`}
+                key={element.number}>
+                {element.name}</NavLink></td>
+            <td>{element.amount}</td>
+            <td>{element.due}</td>
+        </tr>
+    ));
 
-  return (
-    <div style={{ display: "flex" }}>
-      <nav style={{
-        borderRight: "solid 1px",
-        padding: "1rem"
-      }}>
-        <Table highlightOnHover>
-          <thead>
-            <tr>
-              <th>Movie Name</th>
-              <th>Movie Amount</th>
-              <th>Movie Due</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </Table>
-      </nav>
-      <Outlet />
-    </div>
-  );
+    return (
+        <div style={{ display: "flex" }}>
+            <nav style={{
+                borderRight: "solid 1px",
+                padding: "1rem"
+            }}>
+                <Table highlightOnHover>
+                    <thead>
+                        <tr>
+                            <th>Movie Name</th>
+                            <th>Movie Amount</th>
+                            <th>Movie Due</th>
+                        </tr>
+                    </thead>
+                    <tbody>{rows}</tbody>
+                </Table>
+                <Pagination page={activePage} onChange={setPage} total={2} color="dark" sibilings={1} withEdges />
+            </nav>
+            <Outlet />
+        </div>
+    );
 }
