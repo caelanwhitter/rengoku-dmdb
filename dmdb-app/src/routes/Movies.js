034789c340d191308745de1747a2766318983b9b
Daniel Lam <lam.daniel.123@hotmail.com>
2022-02-27 19:56:35 -0500

    Add small changes and comments

--- dmdb-app/src/routes/Movies.js
+++ dmdb-app/src/routes/Movies.js
@@ -2,39 +2,67 @@ import { NavLink, Outlet } from "react-router-dom";
 import { Table, Pagination } from '@mantine/core';
 import React, {useEffect, useState} from 'react'
 
-//This function is used to fetch the data from the server and insert it into a variable called backendData.
+/**
+ * Movies() is a component that fetches the list of Movies from the DB and displays it properly using pagination
+ * @returns Table Of Movies + Pagination
+ */
 export default function Movies() {
+
+    //Initializes variables and sets up "settters to variables"
     const [movies, setMovies] = useState([{}]);
     const [activePage, setPage] = useState(1);
     const [totalPagination, setTotalPagination] = useState();
 
+    /**
+     * useEffect() runs following methods once. Similar to ComponentDidMount()
+     */
+    useEffect(() => {
+        fetchMoviesPerPage(activePage);
+    }, []);
+
+    /**
+     * fetchMoviesPerPage() fetches list of movies following pagination endpoints and calculates totalPagination on first render
+     * @param {*} pageNumber 
+     */
     async function fetchMoviesPerPage(pageNumber) {
         let response = await fetch('http://localhost:3001/api/allMovies/page/' + pageNumber);
         let moviesPaginationJson = await response.json();
         setMovies(moviesPaginationJson);
 
+        // Calls calculateTotalPagination() if totalPagination not initialized yet yet
         if (totalPagination === undefined) {
             await calculateTotalPagination(moviesPaginationJson);
         }
     }
 
+    /**
+     * calculateTotalPagination() calculates how many pages should the entire list of movies be separated for pagination
+     * @param {*} moviesPaginationJson 
+     */
     async function calculateTotalPagination(moviesPaginationJson) {
         let response = await fetch('http://localhost:3001/api/allMovies');
         let allMoviesJson = await response.json();
-        const totalPaginationVar = Math.ceil(allMoviesJson.length/moviesPaginationJson.length);
+        const totalMoviePages = Math.ceil(allMoviesJson.length/moviesPaginationJson.length);
 
-        setTotalPagination(totalPaginationVar);
+        setTotalPagination(totalMoviePages);
     }
 
-    useEffect(() => {
-        fetchMoviesPerPage(activePage);
-    }, []);
-
+    /**
+     * changePage() calls methods whenever detects a change of page on pagination
+     * @param {*} event 
+     */
     const changePage = (event) => {
+
+        // Re-fetches the list of movies with proper page number
         fetchMoviesPerPage(event);
+
+        // Sets activePage to update styles of Pagination
         setPage(event);
     }
 
+    /**
+     * rows returns a table body of the appropriate list of movies
+     */
     const rows = movies.map((element) => (
         <tr key={element._id}>
             <td><NavLink to={`/movies/${element._id}`}
