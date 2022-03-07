65c4d2c01b9f18d6d17217df02359ee8e74fdc71
Danilo Xiaoju Zhu <zhuxiaoj1@outlook.com>
2022-03-07 15:34:33 -0500

    minor changes

--- dmdb-app/src/routes/Home.js
+++ dmdb-app/src/routes/Home.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 //This function is used to render the featured movies of our movie database
 export default function Home() {
   return (
-    <>
       <Container id="mainPageContainer">
         <Group spacing={-100}>
           <Text className="intro" weight={1000} variant="gradient" gradient={{ from: 'dark', to: 'gray', deg: 90}}>Fully Fledged</Text> 
@@ -12,12 +11,14 @@ export default function Home() {
         </Group>
 
         <Text size="xl">The best movie database available to Dawson College - Browse <Space/> 
-          our database of more than 7000 of the most popular movies, rate some, <Space/>
+          our database of more than 7000 of the most popular movies, rate them, <Space/>
           or submit your own Hidden Gem
         </Text>
         <Space h="md"/>
+
         <Button gradient={{ from: 'dark', to: 'gray', deg: 45}} component={Link} to={"/movies"} variant="gradient" size="xl">Start Browsing</Button>
         <Space h="md"/>
+
         <Group>
           <ThemeIcon size="xl"
           variant="gradient" gradient={{ from: 'orange', to: 'yellow', deg: 90}}>
@@ -29,6 +30,5 @@ export default function Home() {
           <a id="gitLabLink" target="_blank" rel="noreferrer" href="https://gitlab.com/zhuxiaoj1/rengoku-dmdb"><Text>Check us out on GitLab!</Text></a>
         </Group>
       </Container>
-    </>
   );
 }
