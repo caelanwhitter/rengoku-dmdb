23549a557e35111cff278dbabe6eef6909f2a86d
Daniel Lam <lam.daniel.123@hotmail.com>
2022-03-18 12:08:05 -0400

    Write unit test:

--- server/__tests__/fetchMovies.test.js
+++ server/__tests__/fetchMovies.test.js
@@ -1,5 +1,16 @@
+const routes = require('../routes/route');
+const supertest = require('supertest');
+const request = supertest(routes);
+
 describe('Sample Test', () => {
     it('should test that true === true', () => {
         expect(true).toBe(true)
+    })
+});
+
+describe("GET/ Request test at endpoint '/oneMovie/fetchMovieDataFromApi/'", () => {
+    it("Tests endpoint with 'The Shining'", async done => {
+        const response = await request.get("/oneMovie/fetchMovieDataFromApi?title=The Shining&year=1980");
+
     })
 })
