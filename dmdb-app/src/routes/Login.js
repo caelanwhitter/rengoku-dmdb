91552191ce8f6aa31418128a44efcbe6c37618ee
Miky Baril <miky.baril1@gmail.com>
2022-03-23 12:21:04 -0400

    Added a .env file in the dmbd-app directory with the google client id. Authentication asks user to login to google

--- dmdb-app/src/routes/Login.js
+++ dmdb-app/src/routes/Login.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import '.././App.css';
+import GoogleLogin, {GoogleLogout} from 'react-google-login';
 
+async function handleLogin(){
+  
+}
 export default function Login() {
   return(
     <div className="login-wrapper">
       <h1>Please Log In</h1>
-      <form>
-        <label>
-          <p>Username</p>
-          <input type="text" />
-        </label>
-        <label>
-          <p>Password</p>
-          <input type="password" />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+      <GoogleLogin
+        clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+        buttonText="Log in with Google"
+        onSuccess={handleLogin}
+        onFailure={handleLogin}
+        cookiePolicy={'single_host_origin'}
+      />
+
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@
  * Start server and listen on port 3001 or PORT environment variable
  * @author Danilo Zhu 1943382
  */
-
+const dotenv = require("dotenv");
+dotenv.config();
 const app = require("./app");
 const PORT = process.env.PORT || 3001;
 
