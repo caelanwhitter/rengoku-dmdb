67d493b9db76775fe8ef4aee89ca89b49e76cba7
Caelan Whitter <caelanwhitter@gmail.com>
2022-03-24 15:24:16 -0400

    App works on local host

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ const app = express();
 const route = require("./routes/route");
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "../dmdb-app/build")));
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
