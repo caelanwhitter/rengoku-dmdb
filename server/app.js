11646a6f9a4a1d15e9320ee1283171b3c42b600b
Caelan Whitter <caelanwhitter@gmail.com>
2022-03-24 14:46:44 -0400

    Added a get in app.js

--- server/app.js
+++ server/app.js
@@ -11,6 +11,10 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "../dmdb-app/build")));
 
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"))
+});
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
