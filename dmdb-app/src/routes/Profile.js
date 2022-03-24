6ad012945e8a42378ee26e93093f5ef75d5dd741
Miky Baril <miky.baril1@gmail.com>
2022-03-24 14:57:13 -0400

    Added google authentication in profile page. Works accordingly

--- dmdb-app/src/routes/Profile.js
+++ dmdb-app/src/routes/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '.././App.css';
 import GoogleLogin, { GoogleLogout } from 'react-google-login';
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function Profile() {
   const [loginData, setLoginData] = useState(
@@ -9,8 +9,8 @@ export default function Profile() {
       ? JSON.parse(localStorage.getItem('loginData'))
       : null
   );
-  const handleFailed = (result) => {
-    alert(result);
+  const handleFailed = () => {
+    alert("Login Failed.");
   };
   const handleLogin = async (googleData) => {
     const res = await fetch('/api/google-login', {
@@ -32,23 +32,35 @@ export default function Profile() {
   }
   return (
     <div className="login-wrapper">
-      <h1>Please Log In</h1>
+
       <div>
         {
           loginData ? (
             <div>
-              <h3>You logged in as {loginData.email}</h3>
-              <button onClick={handleLogout}>Logout</button>
+              <h1 className="centered" > Welcome to the Profile Page! {loginData.name} </h1>
+              <h3 className="centered" >You logged in as {loginData.email}</h3>
+              <h3>
+                <img className="centered" 
+                  src={loginData.picture}
+                  alt="ImageIcon"
+                />
+              </h3>
+              <button 
+                className="centered" 
+                onClick={handleLogout}>Logout</button>
             </div>
           )
             : (
-              <GoogleLogin
-                clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-                buttonText="Log in with Google"
-                onSuccess={handleLogin}
-                onFailure={handleFailed}
-                cookiePolicy={'single_host_origin'}
-              />
+              <div>
+                <h1 className="centered" >Please Log In with your google account</h1>
+                <GoogleLogin
+                  clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                  buttonText="Log in with Google"
+                  onSuccess={handleLogin}
+                  onFailure={handleFailed}
+                  cookiePolicy={'single_host_origin'}
+                />
+              </div>
             )
         }
 
