e0addedd47dd137e5af6f7cacb67a78d797a6e3e
Miky Baril <miky.baril1@gmail.com>
2022-03-24 15:46:27 -0400

    Trying to fix the fetch of the picture

--- dmdb-app/src/routes/Profile.js
+++ dmdb-app/src/routes/Profile.js
@@ -9,8 +9,9 @@ export default function Profile() {
       ? JSON.parse(localStorage.getItem('loginData'))
       : null
   );
-  const handleFailed = () => {
-    alert("Login Failed.");
+  const handleFailed = (result) => {
+    console.log("login failed");
+    //alert(result);
   };
   const handleLogin = async (googleData) => {
     const res = await fetch('/api/google-login', {
@@ -19,6 +20,7 @@ export default function Profile() {
         token: googleData.tokenId,
       }),
       headers: {
+        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
       },
     });
@@ -30,6 +32,16 @@ export default function Profile() {
     localStorage.removeItem('loginData');
     setLoginData(null);
   }
+  async function fetchPicture(picture)
+  {
+    let response = await fetch(picture);
+    if (response.ok)
+    {
+      let pictureblob = await response.blob(); 
+      console.log(pictureblob);
+      return pictureblob;
+    }
+  }
   return (
     <div className="login-wrapper">
 
@@ -37,12 +49,14 @@ export default function Profile() {
         {
           loginData ? (
             <div>
+
               <h1 className="centered" > Welcome to the Profile Page! {loginData.name} </h1>
               <h3 className="centered" >You logged in as {loginData.email}</h3>
+              <h3 className="centered" >You logged in as {loginData.picture}</h3>
               <h3>
                 <img className="centered" 
-                  src={loginData.picture}
-                  alt="ImageIcon"
+                  src={async () => await fetchPicture(loginData.picture)}
+                  alt="NoImage"
                 />
               </h3>
               <button 
@@ -52,8 +66,9 @@ export default function Profile() {
           )
             : (
               <div>
-                <h1 className="centered" >Please Log In with your google account</h1>
+                <h1 className="centered" >Please Log In with your Google account</h1>
                 <GoogleLogin
+                  className="centered"
                   clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
                   buttonText="Log in with Google"
                   onSuccess={handleLogin}
