f3d99780e8de088e71e2f2a636caf75c2eeaf7df
Danilo Xiaoju Zhu <zhuxiaoj1@outlook.com>
2022-04-01 19:42:59 -0400

    Add form validation and regex

--- dmdb-app/src/routes/HiddenGems.js
+++ dmdb-app/src/routes/HiddenGems.js
@@ -1,9 +1,10 @@
 import {
   Badge, Button, Card, Drawer, Grid, Group,
   Modal, NativeSelect, NumberInput, Select,
-  Space, Text, Textarea, TextInput, Title
+  Text, Textarea, TextInput, Title
 } from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
+import { useForm } from '@mantine/form';
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 import { useState } from 'react';
 import { Link } from "react-router-dom";
@@ -14,6 +15,14 @@ export default function HiddenGems() {
   const [opened, setOpened] = useState(false);
   const [addOpened, setAddOpened] = useState(false);
   const [searchopened, setSearchOpened] = useState(false);
+
+  // const [hgTitle, setHgTitle] = useState("");
+  // const [hgDirector, setHgDirector] = useState("");
+  // const [hgDescription, setHgDescription] = useState("");
+  // const [hgDuration, setHgDuration] = useState("");
+  // const [hgRating, setHgRating] = useState("");
+  // const [hgDate, setHgDate] = useState("");
+  // const [hgLink, setHgLink] = useState("");
 
   let elems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   let cards = elems.map((elem) => {
@@ -32,6 +41,23 @@ export default function HiddenGems() {
         </Card>
       </Grid.Col>
     );
+  });
+
+  const form = useForm({
+    initialValues: {
+      title: '',
+      director: '',
+      description: '',
+      duration: null,
+      rating: '',
+      releaseDate: '',
+      link: ''
+    },
+
+    validate: {
+      link: (value) => /(http:\/\/|https:\/\/)?www\.(\w+?)\.(\w+)/g.test(value) 
+        ? null : 'Invalid link!',
+    },
   });
 
   return (
@@ -143,73 +169,76 @@ export default function HiddenGems() {
         size="40%"
       >
         <Text color="red">All fields with an asterisk (*) are required.</Text>
-        <Group
-          direction="column"
-          grow
-        >
+        <form onSubmit={form.onSubmit((values) => console.info(values))}>
           <Group
-            direction="row"
+            direction="column"
             grow
           >
-            <TextInput
-              placeholder="Title"
-              label="Hidden Gem Title"
-              description="Title of the movie"
-              required
-            />
+            <Group grow>
+              <TextInput
+                placeholder="Title"
+                label="Hidden Gem Title"
+                description="Title of the movie"
+                required
+                {...form.getInputProps('title')}
+              />
 
-            <TextInput
-              placeholder="Director"
-              label="Hidden Gem Director"
-              description="Director of the movie"
-            />
-          </Group>
+              <TextInput
+                placeholder="Director"
+                label="Hidden Gem Director"
+                description="Director of the movie"
+                {...form.getInputProps('director')}
+              />
+            </Group>
           
-          <Textarea
-            placeholder="Description"
-            label="Hidden Gem Description"
-            description="Describe your movie"
-          />
-
-          <Group
-            direction="row"
-            grow
-          >
-            <NumberInput
-              label="Hidden Gem Duration"
-              description="Duration of the movie in minutes"
-              placeholder="Duration"
-              max={500}
-              min={10}
-              step={10}
+            <Textarea
+              placeholder="Description"
+              label="Hidden Gem Description"
+              description="Describe your movie"
+              {...form.getInputProps('description')}
             />
 
-            <Select
-              data={['PG', 'PG-13', 'R']}
-              placeholder="Age Rating"
-              label="Hidden Gem Age Rating"
-              description="Age rating of the movie"
-              required
-            />
+            <Group grow>
+              <NumberInput
+                label="Hidden Gem Duration"
+                description="Duration of the movie in minutes"
+                placeholder="Duration"
+                max={500}
+                min={10}
+                step={10}
+                {...form.getInputProps('duration')}
+              />
+
+              <Select
+                data={['PG', 'PG-13', 'R']}
+                placeholder="Age Rating"
+                label="Hidden Gem Age Rating"
+                description="Age rating of the movie"
+                required
+                {...form.getInputProps('rating')}
+              />
+            </Group>
+
+            <Group grow>
+              <DatePicker
+                placeholder="Release Date"
+                label="Hidden Gem Release Date"
+                description="Release date of the movie"
+                required
+                {...form.getInputProps('releaseDate')}
+              />
+
+              <TextInput
+                placeholder="Link"
+                label="Hidden Gem Link"
+                description="Link to the movie"
+                required
+                {...form.getInputProps('link')}
+              />
+            </Group>
+            <Button type="submit" color="dark">Submit</Button>
           </Group>
-
-          <Group grow>
-            <DatePicker
-              placeholder="Release Date"
-              label="Hidden Gem Release Date"
-              description="Release date of the movie"
-              required
-            />
-
-            <TextInput
-              placeholder="Link"
-              label="Hidden Gem Link"
-              description="Link to the movie"
-              required
-            />
-          </Group>
-          <Button color="dark">Submit</Button>
-        </Group>
+        </form>
       </Drawer>
 
       <Grid className="movieGrid" gutter={80}>
